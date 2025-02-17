cae598f2db21eabec83dc80ce017a20002a68cfd
DESKTOP-D91TOJO\c <agsydhf94@gmail.com>
2025-02-18 04:43:10 +0900

    캐릭터 스프라이트 준비,  Idle, Move 애니메이션 작성

---
+++ Assets/Scripts/Player Control/PlayerState.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LF
+{
+    public class PlayerState
+    {
+        protected Player player;
+        protected PlayerStateMachine stateMachine;
+        private string animBoolName;
+
+        // 캐릭터 상태 생성자
+        public PlayerState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName)
+        {
+            this.player = _player;
+            this.stateMachine = _stateMachine;
+            this.animBoolName = _animBoolName;
+        }
+
+
+        // Player.cs 에서 PlayerState 객체를 생성할 때 boolName을 이미 받아온다
+        public virtual void Enter()
+        {
+            player.anim.SetBool(animBoolName, true);
+        }
+
+        public virtual void Update()
+        {
+
+        }
+
+        public virtual void Exit()
+        {
+            player.anim.SetBool(animBoolName, false);
+        }
+    }
+}
