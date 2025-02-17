678190e717ad2e609235003462bdeac4618d34a1
DESKTOP-D91TOJO\c <agsydhf94@gmail.com>
2025-02-18 03:34:55 +0900

    PlayerStateMachine.cs 에서 캐릭터 상태 변화 구조 작성

--- Assets/Player Control/PlayerStateMachine.cs
+++ Assets/Player Control/PlayerStateMachine.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 namespace LF
 {
-    public class PlayerStateMachine : MonoBehaviour
+    public class PlayerStateMachine
     {
+        public PlayerState currentState {  get; private set; }
 
+        public void Initialize(PlayerState _startState)
+        {
+            currentState = _startState;
+            currentState.Enter();
+        }
+
+        public void ChangeState(PlayerState _newState)
+        {
+            currentState.Exit();
+            currentState = _newState;
+            currentState.Enter();
+        }
     }
 }
