9859260b8138d98b9c2c4f6241f6d77a385f380e
DESKTOP-D91TOJO\c <agsydhf94@gmail.com>
2025-02-23 16:14:05 +0900

    플레이어 캐릭터 공격시 정지와 관성표현

--- Assets/Scripts/Player Control/PlayerState/PlayerPrimaryAttackState.cs
+++ Assets/Scripts/Player Control/PlayerState/PlayerPrimaryAttackState.cs
@@ -21,13 +21,16 @@ namespace LF
         {
             base.Enter();
 
+            // 공격 쿨타임 : Time > 마지막 순간의 시간 + 쿨타임 간격
             if(comboCounter > 2 || Time.time >= lastTimeAttacked + comboWindow)
             {
                 comboCounter = 0;
             }
-            Debug.Log(comboCounter);
 
             player.anim.SetInteger("ComboCounter", comboCounter);
+
+            // 공격이 시작되면 제자리에 서기까지 살짝 딜레이를 주어서 관성을 표현
+            stateTimer = 0.1f;
         }
 
         public override void Exit()
@@ -36,12 +39,16 @@ namespace LF
 
             comboCounter++;
             lastTimeAttacked = Time.time;
-            Debug.Log(lastTimeAttacked);
         }
 
         public override void Update()
         {
             base.Update();
+
+            if(stateTimer < 0)
+            {
+                rb.velocity = new Vector2(0, 0);
+            }
 
             if(triggerCalled)
             {
