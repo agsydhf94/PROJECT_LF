0bba0e52ff31bf1d50b8fa414d4676cd8261e9c9
DESKTOP-D91TOJO\c <agsydhf94@gmail.com>
2025-02-23 16:08:04 +0900

    플레이어 캐릭터 콤보 로직 작성

--- Assets/Scripts/Player Control/PlayerState/PlayerPrimaryAttackState.cs
+++ Assets/Scripts/Player Control/PlayerState/PlayerPrimaryAttackState.cs
@@ -7,6 +7,12 @@ namespace LF
 
     public class PlayerPrimaryAttackState : PlayerState
     {
+        private int comboCounter;
+
+        private float lastTimeAttacked;
+        private float comboWindow = 2;
+
+
         public PlayerPrimaryAttackState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
         {
         }
@@ -14,11 +20,23 @@ namespace LF
         public override void Enter()
         {
             base.Enter();
+
+            if(comboCounter > 2 || Time.time >= lastTimeAttacked + comboWindow)
+            {
+                comboCounter = 0;
+            }
+            Debug.Log(comboCounter);
+
+            player.anim.SetInteger("ComboCounter", comboCounter);
         }
 
         public override void Exit()
         {
             base.Exit();
+
+            comboCounter++;
+            lastTimeAttacked = Time.time;
+            Debug.Log(lastTimeAttacked);
         }
 
         public override void Update()
