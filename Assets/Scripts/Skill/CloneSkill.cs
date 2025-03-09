302742d074f28ad03ef669ffd760dd7891d96bc9
YoungHoon Kang <agsydhf94@gmail.com>
2025-03-09 12:04:46 +0900

    플레이어 캐릭터 클론의 공격, 클론 생성시 클론이 가장 가까이에 있는 적을 바라보도록 구현

--- Assets/Scripts/Skill/CloneSkill.cs
+++ Assets/Scripts/Skill/CloneSkill.cs
@@ -9,12 +9,14 @@ namespace LF
         [Header("Clone Information")]
         [SerializeField] private GameObject clonePrefab;
         [SerializeField] private float cloneDuration;
+        [Space]
+        [SerializeField] private bool canAttack;
 
 
         public void CreateClone(Transform clonePosition)
         {
             GameObject newClone = Instantiate(clonePrefab);
-            newClone.GetComponent<CloneSkillController>().SetupClone(clonePosition, cloneDuration);
+            newClone.GetComponent<CloneSkillController>().SetupClone(clonePosition, cloneDuration, canAttack);
         }
     }
 }
